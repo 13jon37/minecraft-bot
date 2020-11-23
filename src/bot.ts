b47de7b79b4987b8c9ef176bdf5beaf1eff84221
13jon37 <jonlockllyr2@gmail.com>
2020-11-23 00:15:53 -0500

    e

--- src/bot.ts
+++ src/bot.ts
@@ -1,19 +1,19 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
-const bot = new Discord.Client()
+const bot = new Discord.Client();
 
-const ping = require('minecraft-server-util')
+const ping = require('minecraft-server-util');
 
-const token = ''
+const token = '';
 
-const PREFIX = '!'
+const PREFIX = '!';
 
 bot.on('ready', () => {
-    console.log('Bot online.')
-})
+    console.log('Bot online.');
+});
 
 bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(' ')
+    let args = message.content.substring(PREFIX.length).split(' ');
 
     switch(args[0]) {
         case 'mc':
@@ -24,16 +24,24 @@ bot.on('message', message => {
                     .addField('Server IP', response.host)
                     .addField('Server Version', response.version)
                     .addField('Online Players', response.onlinePlayers)
-                    .addField('Max Players', response.maxPlayers)
+                    .addField('Max Players', response.maxPlayers);
     
-                    message.channel.send(Embed)
+                    message.channel.send(Embed);
                  })
                 .catch((error) => {
                     throw error;
-            })
-        break
+            });
+        break;
+
+        case 'ip':
+            message.channel.send('nullisblack.mooo.com');
+        break;
+
+        case 'git':
+            message.channel.send("https://github.com/13jon37/minecraft-bot");
+        break;
     }
     
-})
+});
 
-bot.login(token)
+bot.login(token);
